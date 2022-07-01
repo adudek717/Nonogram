0e1787f15806ec03a7cac4f5888e8cb6c23aee86
adudek717 <a.dudek717@gmail.com>
2022-07-01 20:49:17 +0200

    Added cost function and solution-to-problem functions

--- nonogram.cpp
+++ nonogram.cpp
@@ -12,9 +12,9 @@ using namespace std;
 random_device rd;
 mt19937 random_generator(rd());
 
-auto arg = [](int argc, char** argv, std::string name, auto default_value) -> decltype(default_value)
+auto arg = [](int argc, char **argv, std::string name, auto default_value) -> decltype(default_value)
 {
-    //using namespace std;
+    // using namespace std;
     string paramname = "";
     any ret = default_value;
     for (auto argument : vector<string>(argv, argv + argc))
@@ -50,7 +50,7 @@ vector<vector<int>> load_problem(string fname)
 {
     cout << "Loading problem..." << endl;
     vector<vector<int>> problem;
-    istream* fst = &cin;
+    istream *fst = &cin;
     ifstream f;
     if (fname != "")
     {
@@ -72,11 +72,11 @@ vector<vector<int>> load_problem(string fname)
             if ((pos = text.find(delimiter)) != string::npos)
             {
                 token = text.substr(0, pos);
-                //cout << token << endl; // Test output
+                // cout << token << endl; // Test output
                 nums.push_back(stoi(token));
                 text.erase(0, pos + delimiter.length());
             }
-            //cout << "TEXT: " << text << endl; // Test output
+            // cout << "TEXT: " << text << endl; // Test output
             nums.push_back(stoi(text));
             problem.push_back(nums);
         }
@@ -88,7 +88,7 @@ vector<vector<int>> load_valid_solution(string fname)
 {
     cout << "Loading valid solution..." << endl;
     vector<vector<int>> valid_solution;
-    istream* fst = &cin;
+    istream *fst = &cin;
     ifstream f;
     if (fname != "")
     {
@@ -112,29 +112,21 @@ vector<vector<int>> load_valid_solution(string fname)
     return valid_solution;
 }
 
-vector<int> mark_ones(vector<int> vec)
-{
-    return {};
-}
-
-vector<vector<int>> generate_random_solution(vector<vector<int>> problem, vector<vector<int>> valid_solution)
+vector<vector<int>> generate_random_solution(vector<vector<int>> problem, const int rows, const int cols)
 {
     vector<vector<int>> random_solution;
-    int rows = static_cast<int>(valid_solution.size());
-    int cols = static_cast<int>(valid_solution.at(0).size());
-   
 
     // Initialize random_solution vector
-    for (int i = 0; i < valid_solution.size(); i++)
+    for (int i = 0; i < rows; i++)
     {
-        vector<int> col(valid_solution.at(i).size(), 0);
+        vector<int> col(cols, 0);
         random_solution.push_back(col);
     }
 
     // Mark ones for the rows
     // Looping valid solution rows
     // i = problem vector   AND     i = valid_solution vector
-    for (int i = 0; i < rows; i++)  // 0-10
+    for (int i = 0; i < rows; i++) // 0-10
     {
         int offset = 0;
         int sum = 0;
@@ -147,11 +139,12 @@ vector<vector<int>> generate_random_solution(vector<vector<int>> problem, vector
         int range = cols - sum;
 
         // range is a index
-        if (--range < 0) range = 0;
+        if (--range < 0)
+            range = 0;
 
         uniform_int_distribution<int> distr(0, range);
         offset = distr(random_generator);
-        cout << "Offset: " << offset << endl;
+        // cout << "Offset: " << offset << endl;
 
         // Index idx
         int idx = 0;
@@ -163,16 +156,145 @@ vector<vector<int>> generate_random_solution(vector<vector<int>> problem, vector
                 // Looping through the value of the element
                 for (int l = 0; l < problem.at(i).at(k); l++)
                 {
-                    random_solution.at(i).at(idx+offset) = 1; // later l + offset
+                    random_solution.at(i).at(idx + offset) = 1; // later l + offset
                     idx++;
                 }
                 idx++;
             }
-            
         }
     }
 
     return random_solution;
+}
+
+vector<vector<int>> get_problem_rows_from_solution(vector<vector<int>> solution)
+{
+    vector<vector<int>> problem;
+
+    for (int i = 0; i < solution.size(); i++)
+    {
+        int sum = 0;
+        vector<int> vec;
+        for (int j = 0; j < solution.at(i).size(); j++)
+        {
+            int current_num = solution.at(i).at(j);
+            if (current_num != 0)
+                sum++;
+
+            if (vec.empty() && j == solution.at(i).size() - 1)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+
+            if (current_num == 0 && sum != 0)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+            if (current_num != 0 && j == solution.at(i).size() - 1)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+        }
+        problem.push_back(vec);
+    }
+
+    return problem;
+}
+
+vector<vector<int>> get_problem_cols_from_solution(vector<vector<int>> solution)
+{
+    vector<vector<int>> problem;
+
+    for (int i = 0; i < solution.at(0).size(); i++)
+    {
+        int sum = 0;
+        vector<int> vec;
+        for (int j = 0; j < solution.size(); j++)
+        {
+            int current_num = solution.at(j).at(i);
+            if (current_num != 0)
+                sum++;
+
+            if (vec.empty() && j == solution.size() - 1)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+
+            if (current_num == 0 && sum != 0)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+            if (current_num != 0 && j == solution.size() - 1)
+            {
+                vec.push_back(sum);
+                sum = 0;
+            }
+        }
+        problem.push_back(vec);
+    }
+    return problem;
+}
+
+vector<vector<vector<int>>> generate_neighbors(vector<vector<int>> solution, const int rows, const int cols)
+{
+    vector<vector<vector<int>>> neighbors;
+    for (int i = 0; i < solution.size(); i++)
+    {
+        vector<vector<int>> neighbor = solution;
+    }
+    return neighbors;
+}
+
+// Return the cost of a solution
+int get_solution_cost(vector<vector<int>> solution, vector<vector<int>> problem_rows, vector<vector<int>> problem_cols)
+{
+    int overall_cost = 0;
+    int rows_cost = 0;
+    int cols_cost = 0;
+
+    vector<vector<int>> solution_to_problem_rows = get_problem_rows_from_solution(solution);
+    vector<vector<int>> solution_to_problem_cols = get_problem_cols_from_solution(solution);
+
+    for (int i = 0; i < solution_to_problem_rows.size(); i++)
+    {
+        for (int j = 0; j < solution_to_problem_rows.at(i).size(); j++)
+        {
+            if (solution_to_problem_rows.at(i).size() == problem_rows.at(i).size())
+            {
+                if (solution_to_problem_rows.at(i).at(j) != problem_rows.at(i).at(j))
+                    rows_cost++;
+            }
+            else
+            {
+                rows_cost++;
+            }
+        }
+    }
+
+    for (int i = 0; i < solution_to_problem_cols.size(); i++)
+    {
+        for (int j = 0; j < solution_to_problem_cols.at(i).size(); j++)
+        {
+            if (solution_to_problem_cols.at(i).size() == problem_cols.at(i).size())
+            {
+                if (solution_to_problem_cols.at(i).at(j) != problem_cols.at(i).at(j))
+                    cols_cost++;
+            }
+            else
+            {
+                cols_cost++;
+            }
+        }
+    }
+
+    overall_cost = rows_cost + cols_cost;
+
+    return overall_cost;
 }
 
 void print_problem(vector<vector<int>> problem)
@@ -205,28 +327,46 @@ void print_solution(vector<vector<int>> solution)
 int main(int argc, char **argv)
 {
     // get arguments
-    auto fname = arg(argc, argv, "fname", string(""));
-    auto vsol = arg(argc, argv, "vsol", string(""));
-    cout << "# fname = " << fname << ";" << endl;
-    cout << "# vsol = " << vsol << ";" << endl;
+    auto fname_rows = arg(argc, argv, "fname_rows", string(""));
+    auto fname_cols = arg(argc, argv, "fname_cols", string(""));
+    cout << "# fname_rows = " << fname_rows << ";" << endl;
+    cout << "# fname_cols = " << fname_cols << ";" << endl;
 
     // Load the nonogram problem
-    vector<vector<int>> problem = load_problem(fname);
+    vector<vector<int>> problem_rows = load_problem(fname_rows);
 
     // Load the valid solution
-    vector<vector<int>> valid_solution = load_valid_solution(vsol);
+    vector<vector<int>> problem_cols = load_problem(fname_cols);
+
+    // get rozmiar of nonogram
+    const int rows = problem_rows.size();
+    const int cols = problem_cols.size();
 
     // Generate random solution test...
-    vector<vector<int>> random_solution = generate_random_solution(problem, valid_solution);
+    vector<vector<int>> random_solution = generate_random_solution(problem_rows, rows, cols);
+
+    // Generate problem from solution rows test...
+    vector<vector<int>> problem_from_random_solution_rows = get_problem_rows_from_solution(random_solution);
+
+    // Generate problem from solution rows test...
+    vector<vector<int>> problem_from_random_solution_cols = get_problem_cols_from_solution(random_solution);
 
     // Testing problem contents...
-    print_problem(problem);
+    cout << "Initial rows ";
+    print_problem(problem_rows);
 
-    // Testing valid solution contents...
-    print_solution(valid_solution);
+    cout << "Initial cols ";
+    print_problem(problem_cols);
+
+    cout << "From solution rows ";
+    print_problem(problem_from_random_solution_rows);
+
+    cout << "From solution cols ";
+    print_problem(problem_from_random_solution_cols);
 
     // Testing random solution contents...
     cout << "Random ";
     print_solution(random_solution);
+
+    cout << "Random solution cost: " << get_solution_cost(random_solution, problem_rows, problem_cols) << endl;
 }
- 
